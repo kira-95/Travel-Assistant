4f292562d13c5b9a8f88a3dfccb6c28d4da13434
git config --global user.email "linkaikai28@126.com <shaokai_lin@126.com>
2018-10-30 21:53:00 -0400

    modified analysis

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -1,2 +1,2 @@
 today=`date '+%Y_%m_%d_%H:%M:%S'`;
-sh ../pmd-bin-6.9.0/bin/run.sh pmd -d ../src/ -f text -R rulesets/java/basic.xml > ../logs/$today.static_analysis.txt
+sh ../pmd-bin-6.9.0/bin/run.sh pmd -d ../src/ -f text -R rulesets/java/basic.xml > ./logs/$today.static_analysis.txt
