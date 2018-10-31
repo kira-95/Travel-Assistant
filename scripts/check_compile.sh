0c0338511a67131624bdaf1ffa8b698305defb55
git config --global user.email "linkaikai28@126.com <shaokai_lin@126.com>
2018-10-30 22:36:00 -0400

    add logs

--- scripts/check_compile.sh
+++ scripts/check_compile.sh
@@ -1,4 +1,7 @@
-success=$( javac ./src/Main.java ./src/Controller.java )
+success=$( javac -d ./bin/temp ./src/Main.java ./src/Controller.java )
+
+# remove build files
+rm ./bin/temp/*.class
 
 if [ -z "$success" ]; then
 	echo 0
