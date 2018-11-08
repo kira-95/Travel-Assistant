7158e2d1d874af71d369555438afe34fc5160c25
Xijie Guo <guo26x@mtholyoke.edu>
2018-11-08 10:21:54 -0500

    Added SpotsCollectionTest

--- test/SpotsCollectionTest.java
+++ test/SpotsCollectionTest.java
@@ -17,16 +17,6 @@ public class SpotsCollectionTest {
         spots.addSpot("Central Park");
         spots.addSpot("Empire State Building");
         spots.addSpot("SOHO");
-
-        double[][] distanceMatrix = {
-                {0.0, 10.0, 8.0, 7.0, 2.0},
-                {10.0, 0.0, 9.0, 11.0, 19.0},
-                {8.0, 9.0, 0.0, 4.0, 18.0},
-                {7.0, 11.0, 4.0, 0.0, 27.0},
-                {2.0, 19.0, 18.0, 27.0, 0.0}
-        };
-
-        spots.setDistanceMatrix(distanceMatrix);
     }
 
     /**
